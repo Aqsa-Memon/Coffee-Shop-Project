40b738d031254b53b9f69aef958da8c9847811e0
Aqsa-Memon <muhammadzaidjati2@gmail.com>
2024-10-03 02:18:58 +0500

    Project

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,61 @@
+
+// import React from 'react'
+
+
+// function Home() {
+//   return (
+//     <div>
+       
+//         <main>
+//       <img className=' className="relative z-10 flex flex-col justify-center items-center h-screen text-center text-white bg-black bg-opacity-50'src="https://static.vecteezy.com/system/resources/thumbnails/023/010/450/small_2x/the-cup-of-latte-coffee-with-heart-shaped-latte-art-and-ai-generated-free-photo.jpg" alt="" />
+    
+//       </main>
+
+//       <main>
+
+
+//       <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-40 text-white">
+//           <h1 className="text-5xl font-bold mb-4">Welcome to Coffee Shop</h1>
+//           <p className="text-xl mb-8">Your daily dose of energy and warmth, right here.</p>
+//           <button className="bg-yellow-600 text-white py-2 px-6 rounded-lg hover:bg-yellow-500">
+//             Order Now
+//           </button>
+//         </div>
+//       </main>
+
+//       <footer className="text-center py-6 bg-yellow-600 text-white">
+//         <p>&copy; 2024 Coffee Shop. All rights reserved.</p>
+//       </footer> 
+    
+//       </div>
+    
+//   )
+  
+// }
+// export default Home
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react'
+
+function CONTACT() {
+  return (
+    <div>
+      <h1 className='text-decoration-line: underline text-center m-5'>THIS IS CONTACT PAGE </h1></div>
+  )
+}
+
+export default CONTACT
